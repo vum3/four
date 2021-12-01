499765e5cd0c95a17119e67e399fe9e38e5d2d9d
Shin yu <10930287@dcsh.tp.edu.tw>
2021-12-01 12:07:09 +0800

    12/01

--- forum/settings.py
+++ forum/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'forum.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [pagetmpl],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
